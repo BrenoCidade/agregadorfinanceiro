a9d6e3850cddb02a412fc8e41a55ac02af675e43
Breno Cidade <brenocidade01@gmail.com>
2024-06-03 20:14:31 -0300

    first commit

---
+++ agregadorfinanceiro/src/main/java/com/brenocidade/agregadorfinanceiro/AgregadorfinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.brenocidade.agregadorfinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorfinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorfinanceiroApplication.class, args);
+	}
+
+}
---
+++ agregadorfinanceiro/src/test/java/com/brenocidade/agregadorfinanceiro/AgregadorfinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenocidade.agregadorfinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorfinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
